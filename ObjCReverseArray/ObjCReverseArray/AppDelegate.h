15a5b7dbc7d44557db060893beb55797e4ef2ce1
Emily Mearns <mearnsem@gmail.com>
2016-07-13 09:10:40 -0600

    Stretch Problems

---
+++ ObjCReverseArray/ObjCReverseArray/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCReverseArray
+//
+//  Created by Emily Mearns on 7/13/16.
+//  Copyright © 2016 Emily Mearns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjCReverseArray/ObjCReverseArray/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjCReverseArray
+//
+//  Created by Emily Mearns on 7/13/16.
+//  Copyright © 2016 Emily Mearns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
