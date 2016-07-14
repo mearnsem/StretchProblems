5e90f1d1f53dce9f356adacfd24c987e40f3c906
Emily Mearns <mearnsem@gmail.com>
2016-07-14 09:06:33 -0600

    ObjC

---
+++ ObjCDivisible/ObjCDivisible/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCDivisible
+//
+//  Created by Emily Mearns on 7/14/16.
+//  Copyright © 2016 Emily Mearns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
